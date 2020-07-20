e7093781e83294ccc66398109546417f3b17277d
Mathias Sirvent <sirvent@posteo.de>
2020-07-20 21:07:50 +0200

    init

--- schafkopf/database/session.py
+++ schafkopf/database/session.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+import os
 
 class Sessions:
 
     @staticmethod
     def get_session():
-        engine = create_engine("sqlite:///schafkopf/schafkopf.db")
+        DATABASE_URL = os.environ['DATABASE_URL']
+        engine = create_engine(DATABASE_URL)
         my_session = sessionmaker(bind=engine)
         return my_session()
