f7b28f9e2a388d86407286780b5b8ee03f0f6348
Mathias Sirvent <sirvent@posteo.de>
2020-07-22 15:02:43 +0200

    Trying ...

--- schafkopf/database/session.py
+++ schafkopf/database/session.py
@@ -8,6 +8,7 @@ from sqlalchemy.pool import NullPool, QueuePool
 
 class Sessions:
     # engine = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+    # engine = create_engine(os.environ['DATABASE_URL'], poolclass=NullPool)
     engine = create_engine(os.environ['DATABASE_URL'], poolclass=QueuePool, pool_size=20, max_overflow=0)
 
     @staticmethod
