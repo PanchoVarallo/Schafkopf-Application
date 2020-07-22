7f5f202161be6a1e3fe5f61fcaccf52fcf694218
Mathias Sirvent <sirvent@posteo.de>
2020-07-22 13:48:07 +0200

    Trying Queuepool

--- schafkopf/database/session.py
+++ schafkopf/database/session.py
@@ -3,12 +3,12 @@ import os
 from sqlalchemy import create_engine
 import psycopg2
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
+from sqlalchemy.pool import NullPool, QueuePool
 
 
 class Sessions:
     # engine = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
-    engine = create_engine(os.environ['DATABASE_URL'], poolclass=NullPool)
+    engine = create_engine(os.environ['DATABASE_URL'], poolclass=QueuePool)
 
     @staticmethod
     def get_session():
