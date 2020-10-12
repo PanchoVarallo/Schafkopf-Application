164081be9e285840f56179dec72137fbd85ea647
Mathias Sirvent <sirvent@posteo.de>
2020-10-12 12:12:42 +0200

    Working on settings

--- schafkopf/database/session.py
+++ schafkopf/database/session.py
@@ -7,15 +7,15 @@ from sqlalchemy.pool import NullPool, QueuePool
 
 
 class Sessions:
-    engine = create_engine(os.environ['DATABASE_URL'], poolclass=QueuePool, pool_size=15, max_overflow=0)
-    # if os.environ['ENVIRONMENT'] is None:
-    #     # On Heroku Server
-    # elif os.environ['ENVIRONMENT'] == 'SQLITE_DATABASE':
-    #     # Using sqlite
-    #     engine = create_engine(os.environ['DATABASE_URL'], poolclass=NullPool)
-    # elif os.environ['ENVIRONMENT'] == 'HEROKU_DATABASE':
-    #     # Using the Heroku postgres database locally
-    #     engine = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
+    if os.environ['ENVIRONMENT'] == 'PRODUCTION':
+        # On Heroku Server
+        engine = create_engine(os.environ['DATABASE_URL'], poolclass=QueuePool, pool_size=15, max_overflow=0)
+    if os.environ['ENVIRONMENT'] == 'SQLITE_DATABASE':
+        # Using sqlite
+        engine = create_engine(os.environ['DATABASE_URL'], poolclass=NullPool)
+    elif os.environ['ENVIRONMENT'] == 'HEROKU_DATABASE':
+        # Using the Heroku postgres database locally
+        engine = psycopg2.connect(os.environ['DATABASE_URL'], sslmode='require')
 
     @staticmethod
     def get_session():
